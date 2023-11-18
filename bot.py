16aaf09e52ec23415a1813c152c3d6634ebd5bc1
Rauan Pires <rauan_pires@riseup.net>
2023-11-18 09:16:28 -0300

    Updates comp command help string

--- bot.py
+++ bot.py
@@ -264,7 +264,7 @@ async def five_vs_five(ctx, command=None):
         await ctx.send(f"{ctx.author.mention}, você já está na lista **{insult}** 🙄")
 
 
-@bot.command(name='comp', help="Busca uma comp para jogar")
+@bot.command(name='comp', help="!comp <mapa> || random")
 async def comp_maker(ctx, command=None):
     api_url = "https://api.thespike.gg/stats/compositions?"
     if command == "random":
